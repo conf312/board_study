a25df73694ee53bc7b3482f8f2a8dcf49c073a63
venganza312 <gpfm312@gmail.com>
2021-11-17 15:48:27 +0900

    update login process

--- src/main/java/com/board/study/web/BoardController.java
+++ src/main/java/com/board/study/web/BoardController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-
 import com.board.study.dto.board.BoardRequestDto;
 import com.board.study.service.BoardService;
-
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
